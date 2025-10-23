c3da994f90f5cfd64f0960b4d6610c92413f4059
TeclaColesa <tecla.colesa@stud.ubbcluj.ro>
2025-10-23 23:53:56 +0300

    Update model_suggestions.py

--- model_suggestions.py
+++ model_suggestions.py
@@ -1,12 +1,1 @@
-#SUGGEST SUITABLE ML MODELS
-def suggest_models(target_type, n_samples, n_features):
-    if target_type == 'classification':
-        if n_samples < 10000:
-            return ["LogisticRegression", "RandomForestClassifier"]
-        else:
-            return ["LightGBM", "XGBoost"]
-    else:
-        if n_samples < 10000:
-            return ["LinearRegression", "RandomForestRegressor"]
-        else:
-            return ["LightGBMRegressor", "XGBoostRegressor"]
+
